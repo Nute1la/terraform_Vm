c704e91c42b1d91332e728b368b2c14112a21e06
nute1la <natasha_savchenko16@mail.ru>
2021-11-11 16:19:56 +0300

    changes

--- Modules/Vm/main.tf
+++ Modules/Vm/main.tf
@@ -1,35 +1,47 @@
 resource "google_compute_instance" "kotik_Vm" {
-  name           = "kotik_Vm"
-  machine_type   = "e2-small"
-  zone           = "us-central1-a"
-  desired_status = "RUNNING"
+  name           = var.name
+  machine_type   = var.machine_type
+  zone           = var.zone
+  desired_status = var.desired_status
 
   network_interface {
-    network = "default"
+    network = var.network
   }
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = var.image
     }
+  }
+
+  metadata = {
+    ssh-keys = "unit:${tls_private_key.example.public_key_pem}"
   }
 }
 
 resource "google_compute_firewall" "http-server" {
-  name    = "default-allow-http"
-  network = "default"
+  name    = "${var.name}allow-http"
+  network = var.network
 
   allow {
-    protocol = "tcp"
-    ports    = ["80", "22", "443"]
+    protocol = var.protocol
+    ports    = var.ports
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
 }
 
 resource "tls_private_key" "example" {
-  algorithm   = "RSA"
-  rsa_bits = "2048"
+  algorithm = var.algorithm
+  rsa_bits  = var.rsa_bits
 }
 
+resource "local_file" "key" {
+  content = templatefile("${path.module}/templates/private_key.tmpl",
+    {
+    ssh_private_key = tls_private_key.example.private_key_pem
+    }
+  )
+  filename = "private_key"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+name           = "kotik"
+machine_type   = "e2-small"
+zone           = "us-central1-a"
+desired_status = "RUNNING"
+network        = "default"
+image          = "debian-cloud/debian-9"
+protocol       = "tcp"
+ports          = ["80", "22", "443"]
+source_ranges  = ["0.0.0.0/0"]
+target_tags    = ["http-server"]
+algorithm      = "RSA"
+rsa_bits       = "2048"
